7130e81c167f75d1d6dbe414ba445cc68ac5c439
Bran Liang <lby89757@hotmail.com>
2024-07-02 13:25:51 +0800

    Add solution for is sequence

--- lib/q_392_is_subsequence.rb
+++ lib/q_392_is_subsequence.rb
@@ -1,2 +1,18 @@
 module Q392IsSubsequence
+  def is_subsequence(s, t)
+    current = t
+    result = true
+
+    s.chars.each do |c|
+      i = current.index(c)
+      if i == nil
+        result = false
+        break
+      else
+        current = current[(i + 1)..-1]
+      end
+    end
+
+    result
+  end
 end
--- test/q_392_is_subsequence_test.rb
+++ test/q_392_is_subsequence_test.rb
@@ -2,4 +2,16 @@ require_relative "./test_helper"
 
 class Q392IsSubsequenceTest < Minitest::Test
   include Q392IsSubsequence
+
+  def test_case_1
+    assert_equal true, is_subsequence("abc", "ahbgdc")
+  end
+
+  def test_case_2
+    assert_equal false, is_subsequence("axc", "ahbgdc")
+  end
+
+  def test_case_3
+    assert_equal false, is_subsequence("aaaaaa", "bbaaaa")
+  end
 end
